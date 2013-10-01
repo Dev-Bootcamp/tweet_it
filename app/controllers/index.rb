2a8d07bec01d87271d49a410ea19ecd1707c3927
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-01 16:40:30 -0500

    hard-coded tweets working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,17 @@
+# ============= GET ===============
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end
+
+get '/tweeted' do
+  erb :tweeted
+end
+
+# ============== POST ================
+
+post '/tweet' do
+  Twitter.update(params[:tweet])
+  erb :tweeted
 end
